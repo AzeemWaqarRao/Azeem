4b02fde8776e51872e2296c50560c7edeb16889e
AzeemWaqarRao <61060465+AzeemWaqarRao@users.noreply.github.com>
2020-02-15 10:19:11 -0800

    Add files via upload

---
+++ print/src/print/Print.java
@@ -0,0 +1,34 @@
+
+package print;
+
+
+public class Print
+{
+
+  
+    public static void main(String[] args) 
+    {
+        
+        int n=2;
+      //change value of n to demonstrate functioning of switch
+       switch(n)
+       {
+           case 1:
+               System.out.println("not even close to 3");
+               break;
+           case 2:
+               System.out.println(" close to 3");
+               break;
+           case 3:
+               System.out.println("value is 3");
+               break;
+           default:
+               System.out.println("not found");
+              
+
+       }
+     
+
+    }
+    
+}
